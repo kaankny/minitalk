e17378c15f3c128faf81cd5ce60f07f1812e64bc
Kaan Kanyılmaz <62248541+kaankny@users.noreply.github.com>
2023-11-13 17:00:13 +0300

    Add files via upload

---
+++ minitalk/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkanyilm <42kocaeli.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 16:07:51 by kkanyilm          #+#    #+#             */
+/*   Updated: 2023/11/11 11:12:26 by kkanyilm         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+void	ft_putchar(char);
+void	ft_putnbr(int);
+int		ft_atoi(char*);
+void	ft_putlogo(int);
+
+#endif
